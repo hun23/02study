619c3ad2da9b65714eb5468d297b399a8e0f5022
heejinssss <heejin.dev33@gmail.com>
2023-03-30 17:05:16 +0900

    아기상어, 치즈

---
+++ 0330/hj/babyshark.py
@@ -0,0 +1,59 @@
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+cnt = 0 # 섭취한 먹이 수 (성장할 때마다 초기화 예정)
+time = 0
+baby = 2
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# 아기 상어 초기 위치
+for i in range(N):
+    for j in range(N):
+        if arr[i][j] == 9:
+            sx, sy = i, j
+
+def bfs(sx, sy, baby):
+    visited = [[0]*N for _ in range(N)]
+    ans = [[0] * N for _ in range(N)]
+
+    queue = [[sx, sy]]
+    visited[sx][sy] = 1 # 초기 위치 방문 완료 표시
+    cmp = [] # [현재 위치부터 먹을 수 있는 먹이까지의 거리, x, y]
+
+    while queue:
+        cx, cy = queue.pop(0)
+        
+        for i in range(4):
+            nx, ny = cx+dx[i], cy+dy[i]
+            
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == 0:
+                if arr[nx][ny] <= baby:
+                    queue.append([nx, ny])
+                    visited[nx][ny] = 1
+                    ans[nx][ny] = ans[cx][cy] + 1
+                    if arr[nx][ny] < baby and arr[nx][ny] != 0:
+                        cmp.append([ans[nx][ny], nx, ny])
+                        
+    # 거리 기준 오름차순, x값 기준 오름차순 (작은 수부터), y값 기준 오름차순 (작은 수부터)
+    cmp = sorted(cmp, key=lambda x: (x[0], x[1], x[2]))
+    return cmp
+
+while True:
+    mom = bfs(sx, sy, baby)
+
+    if not len(mom):
+        break
+
+    t, nx, ny = mom.pop(0)
+    time += t
+    cnt += 1
+
+    arr[sx][sy], arr[nx][ny] = 0, 0 # 출발한 위치와, 물고기 먹은 위치 0으로 초기화
+    sx, sy = nx, ny # 물고기를 먹은 자리로 현재 위치를 초기화
+
+    if cnt == baby: # 섭취한 먹이 수가 현재 크기와 같다면
+        baby += 1 # 성장
+        cnt = 0 # 섭취한 먹이 수 초기화
+
+print(time)
---
+++ 0330/hj/cheese.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+queue = deque([])
+
+cmp = [sum(arr, []).count(1)] # 한 사이클 돌 때마다 치즈 개수 누적 저장
+cnt = 0
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+while arr != [[0]*m for _ in range(n)]: # 모든 치즈가 녹을 때까지 실행
+
+    visited = [[0] * m for _ in range(n)]
+    queue.append([0, 0])
+
+    while queue:
+        sx, sy = queue.popleft()
+
+        for i in range(4):
+            nx = sx + dx[i]
+            ny = sy + dy[i]
+            # 공기를 만나면 좌표 저장, 방문 체크 완료 후 탐색 진행
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 0 and visited[nx][ny] == 0:
+                queue.append([nx, ny])
+                visited[nx][ny] = 1
+            # 치즈를 만나면 녹인 후, 방문 체크 완료
+            elif 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 1 and visited[nx][ny] == 0:
+                arr[nx][ny] = 0
+                visited[nx][ny] = 1
+
+    cmp.append(sum(arr, []).count(1)) # 치즈 개수 저장
+    cnt += 1
+
+cmp.remove(0) # 모든 치즈가 녹은 후에 치즈 개수를 센 결과는 삭제
+
+print(cnt)
+print(cmp[-1])
